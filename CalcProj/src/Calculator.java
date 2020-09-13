dbb101c26032261b9d007c4b84e8fead8fc0f641
Vy <ks031088@gmail.com>
2020-09-13 21:11:52 +0300

    Added few functions for calc

--- CalcProj/src/Calculator.java
+++ CalcProj/src/Calculator.java
@@ -1,9 +1,31 @@
 
 public class Calculator {
+	/*
+	 * Few strings comment ClassName begin from UpperCase functionName begin from
+	 * lowerCase
+	 * 
+	 */
+
+	public void add() {
+		System.out.println("Adding some numbers");
+	}
+
+	public void sub() {
+		System.out.println("Substract some numbers");
+	}
+
+	public void div() {
+		System.out.println("Divide some numbers");
+	}
+
+	public void mult() {
+		System.out.println("Multiplicate some numbers");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
+//		System.out.println("Hello World 2!!!");
+
 	}
 
 }
